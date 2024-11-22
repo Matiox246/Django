0bdf21f4ec61ee51b72a16849c9570faf6ab654b
Matin <mohamad13840208@gmail.com>
2024-11-22 17:30:28 +0330

    add custom fields to account:admin

--- finance_manager/account/models.py
+++ finance_manager/account/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from datetime import datetime
 # Create your models here.
 
 class User(AbstractUser):
-    is_admin = models.BooleanField(default=False, verbose_name="Membership type")
-    premum_user = models.DateTimeField(default=timezone.now, verbose_name="Membership type")
+    is_author = models.BooleanField(default=False, verbose_name="Is Author")
+    premum_user = models.DateTimeField(default=timezone.now, verbose_name="Is Premum Until")
 
     def is_premum_user(self):
-        if self.premum_user > timezone.now():
+        if self.premum_user and self.premum_user > timezone.now():
             return True
         else:
             return False
+    is_premum_user.boolean = True
+    is_premum_user.short_description = "IS PREMUM USER"
